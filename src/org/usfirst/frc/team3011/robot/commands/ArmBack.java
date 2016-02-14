44302a2e70be3e7a5bb2efe6814eaeba8d4bfaea
Dennis <dennis.sarsozo@gmail.com>
2016-02-14 13:18:49 +0100

    Changed interuppted() commands to do end() in command files.

--- src/org/usfirst/frc/team3011/robot/commands/ArmBack.java
+++ src/org/usfirst/frc/team3011/robot/commands/ArmBack.java
@@ -38,6 +38,6 @@ public class ArmBack extends Command {
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
-    	Robot.arm.stop();
+    	end();
     }
 }
